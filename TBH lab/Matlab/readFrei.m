56ba54ca1695d0a439ab5ea4b186d8ad2d56094c
Dirk <coolice.f.b@web.de>
2012-09-13 15:16:43 +0200

    anfang Messergebnisse

--- TBH lab/Matlab/readFrei.m
+++ TBH lab/Matlab/readFrei.m
@@ -1,3 +1,10 @@
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Times New Roman')
+set(0,'DefaultAxesFontSize', 12)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Times New Roman')
+set(0,'DefaultTextFontSize', 10)
 
 % % Slurp in all lines.
 % f = fopen('../Messwerte/dunkel_sperrsättigungsstrom.txt');
@@ -15,6 +22,8 @@
 % % value
 % b(b(:,2) == 0,2) = realmin;
 
+
+
 [A1.U,A1.I] = readBLA('../Messwerte/Wafer120502_z1s6_Biggi_UI_Kennlinie_neu_gut.txt');
 [A2.U,A2.I] = readBLA('../Messwerte/Wafer120502_z6s6_Biggi_UI_Kennlinie_neu_gut.txt');
 [A3.U,A3.I] = readBLA('../Messwerte/Wafer120502_z12s6_Biggi_UI_Kennlinie_neu_gut.txt');
@@ -28,19 +37,33 @@
 [A8.U,A8.I] = readBLA('../Messwerte/Wafer120502_z7s7_Biggi_UI_Kennlinie_beleuchtet.txt');
 
 
+YLABEL = '| I |   [A]';
+XLABEL = 'U [V]';
 
 A = A1;
-
 I = A.I;
 U = A.U;
 I = abs(I);
+Rmin = find(U > 0.3367, 1);
+Rmax = find(U > 0.4573, 1);
+p = polyfit(U(Rmin:Rmax),log(I(Rmin:Rmax)),1);
+R = logspace(U(Rmin),U(Rmax),(Rmax - Rmin)+1)';
+%R = p(1) .* U + p(2);
+%R = polyval(p, U);
+
 figure(601);
     clf(601)
-        semilogy(U,I,'k');
+       semilogy(U,I,'k');
+
+        
+        hold on
+        semilogy(U(Rmin:Rmax),R,'r');
+        hold off
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
-    ylim([1E-11 1])
+        
+    ylabel(YLABEL)
+    xlabel(XLABEL)
+   % ylim([1E-11 1])
     grid();
     
     
@@ -53,8 +76,8 @@ figure(602);
     clf(602)
         semilogy(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
     ylim([1E-11 1])
     grid();
     
@@ -68,8 +91,8 @@ figure(603);
     clf(603)
         semilogy(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
     ylim([1E-11 1])
     grid();
     
@@ -85,8 +108,8 @@ figure(604);
     clf(604)
         semilogy(U,abs(I-I(U == min(abs(U)))),'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
 %     ylim([1E-11 1])
     grid();
     
@@ -100,8 +123,8 @@ figure(605);
     clf(605)
         semilogy(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
     ylim([1E-11 1])
     grid();
 
@@ -115,8 +138,8 @@ figure(606);
     clf(606)
         semilogy(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
     ylim([1E-11 1])
     grid();
     
@@ -130,8 +153,8 @@ figure(607);
     clf(607)
         semilogy(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
     ylim([1E-11 1])
     grid();
 
@@ -145,8 +168,8 @@ figure(608);
     clf(608)
         plot(U,I,'k');
 %     legend('\rho = -1','\rho = -1/3','\rho = 0')
-    ylabel('I [A]')
-    xlabel('U [V]')
+    ylabel(YLABEL)
+    xlabel(XLABEL)
 %     ylim([1E-11 1])
     grid();
     
